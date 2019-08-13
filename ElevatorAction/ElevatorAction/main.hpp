cf392815bf7c7b0997d97328df175da4ec8d4500
Aleksandar Kondic <konda96@gmail.com>
2019-08-13 02:06:49 +0200

    Initial commit

---
+++ ElevatorAction/ElevatorAction/main.hpp
@@ -0,0 +1,9 @@
+// Function prototypes for main.cpp
+
+void myKeyboardFunc( unsigned char key, int x, int y );
+void mySpecialKeyFunc( int key, int x, int y );
+
+void drawScene(void);
+
+void initRendering();
+void resizeWindow(int w, int h);
