3d1d7e7fb868a20b8ae29dc2f3277b0e301b6e17
Aleksandar Kondic <konda96@gmail.com>
2019-08-14 02:05:28 +0200

    Add passive movement to Elevator

--- ElevatorAction/ElevatorAction/Elevator.cpp
+++ ElevatorAction/ElevatorAction/Elevator.cpp
@@ -1,11 +1,27 @@
+#include <assert.h>
+#include <cmath>
 #include "Elevator.hpp"
 #include "util.hpp"
+
+void Elevator::move_next()
+{
+	assert(this->max_floor > this->min_floor);
+	assert(this->is_moving() == false);
+
+	int next_floor = this->current_floor() + this->passive_direction;
+	if (next_floor > this->max_floor || next_floor < this->min_floor)
+		this->passive_direction *= -1;
+
+	this->move_generic(this->passive_direction);
+}
 
 Elevator::Elevator(int x, int min_floor, int max_floor, float vspeed) : MapObject(x), min_floor(min_floor), max_floor(max_floor), vspeed(vspeed)
 {
 	int start_floor = randint(min_floor, max_floor);
 	this->fy = (float)start_floor;
 	this->target_floor = start_floor;
+
+	this->passive_direction = (randint(0, 1) == 0 ? -1 : 1);
 }
 
 Elevator::~Elevator()
@@ -14,7 +30,10 @@ Elevator::~Elevator()
 
 int Elevator::current_floor()
 {
-	return (int)this->fy;
+	if (abs((float)this->target_floor - this->fy) < 0.01f)
+		return this->target_floor;
+	float trunc_val = (this->direction() == 1 ? std::floor(this->fy) : std::ceil(this->fy));
+	return (int)trunc_val;
 }
 
 bool Elevator::is_moving()
@@ -26,8 +45,10 @@ bool Elevator::move_up()
 {
 	if (this->is_moving() || this->current_floor() >= this->max_floor)
 		return false;
-	else
+	else {
 		this->target_floor = this->current_floor() + 1;
+		this->passive_direction = 1;
+	}
 	return true;
 }
 
@@ -35,20 +56,49 @@ bool Elevator::move_down()
 {
 	if (this->is_moving() || this->current_floor() <= this->min_floor)
 		return false;
-	else
+	else {
 		this->target_floor = this->current_floor() - 1;
+		this->passive_direction = -1;
+	}
 	return true;
+}
+
+bool Elevator::move_generic(int direction)
+{
+	assert(direction == 1 || direction == -1);
+	if (direction == 1)
+		return this->move_up();
+	else
+		return this->move_down();
+}
+
+int Elevator::direction()
+{
+	float diff = (float)this->target_floor - this->fy;
+	if (diff > 0)
+		return 1;
+	else if (diff < 0)
+		return -1;
+	else
+		return this->passive_direction;
 }
 
 void Elevator::process(float delta)
 {
 	if (this->is_moving()) {
-		float sign = (this->target_floor > this->current_floor() ? 1.0f : -1.0f); // Move up or down
+		float sign = (float)this->direction();
 		this->fy += sign * this->vspeed * delta;
 
 		// Correct the fy value to match the exact value of current_floor when elevator stops moving
 		if (!this->is_moving()) {
 			this->fy = (float)this->current_floor();
+		}
+	}
+	else {
+		this->wait_time_elapsed += delta;
+		if (this->wait_time_elapsed >= this->wait_time) {
+			this->wait_time_elapsed = 0.0f;
+			this->move_next();
 		}
 	}
 }
--- ElevatorAction/ElevatorAction/Elevator.hpp
+++ ElevatorAction/ElevatorAction/Elevator.hpp
@@ -3,6 +3,11 @@
 
 class Elevator : public MapObject
 {
+	float wait_time = 2.0f;
+	float wait_time_elapsed = 0.0f;
+	int passive_direction; //1 == up, -1 == down
+
+	void move_next();
 public:
 	float fy;
 	int min_floor;
@@ -17,6 +22,8 @@ public:
 	bool is_moving();
 	bool move_up();
 	bool move_down();
+	bool move_generic(int direction);
+	int direction();
 	void process(float delta) override;
 	void render(float delta) override;
 };
