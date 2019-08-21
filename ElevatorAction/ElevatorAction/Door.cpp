7945da8ab2cdc930f44b0c1520af409398f8c0db
Aleksandar Kondic <konda96@gmail.com>
2019-08-21 02:24:21 +0200

    Add Door

---
+++ ElevatorAction/ElevatorAction/Door.cpp
@@ -0,0 +1,11 @@
+#include "Door.hpp"
+
+
+Door::Door(int x, int y): SingleFloorLevelObject(x, y)
+{
+}
+
+
+Door::~Door()
+{
+}
---
+++ ElevatorAction/ElevatorAction/Door.hpp
@@ -0,0 +1,10 @@
+#pragma once
+#include "SingleFloorLevelObject.hpp"
+class Door :
+	public SingleFloorLevelObject
+{
+public:
+	Door(int x, int y);
+	~Door();
+};
+
