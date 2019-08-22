23021c57c58ea989b9848cf2a5068eb062351a50
Aleksandar Kondic <konda96@gmail.com>
2019-08-22 20:37:27 +0200

    Add level generation code

--- ElevatorAction/ElevatorAction/Level.cpp
+++ ElevatorAction/ElevatorAction/Level.cpp
@@ -1,9 +1,50 @@
 #include "Level.hpp"
+#include "util.hpp"
+#include "DocumentDoor.hpp"
+#include "EnemyDoor.hpp"
 #include <utility>
 #include <exception>
 
 
-Level::Level(int width, int height): width(width), height(height)
+int Level::remaining_free()
+{
+	return this->size() - this->occupied.size();
+}
+
+void Level::generate_missing()
+{
+	this->generate_missing_doors();
+}
+
+void Level::generate_missing_doors()
+{
+	this->insert_document_doors();
+	// Fill up the rest of free space with enemy doors
+	bool b = false; // Break condition
+	for (int y = 1; y <= this->height && !b; y++) {
+		for (int x = 1; x <= this->width && !b; x++) {
+			if (this->occupied.count(std::make_pair(x, y)) == 0) { // Free position
+				this->add_xy(new EnemyDoor(x, y));
+			}
+			b = this->remaining_free() <= 0;
+		}
+	}
+}
+
+void Level::insert_document_doors()
+{
+	int remaining_document = this->document_doors;
+	while (this->remaining_free() > 0 && remaining_document > 0) {
+		int posx, posy;
+		do {	// Find a random free position in the level to insert document doors
+			posx = randint(1, this->width);
+			posy = randint(1, this->height);
+		} while (this->occupied.count(std::make_pair(posx, posy)));
+		this->add_xy(new DocumentDoor(posx, posy));
+	}
+}
+
+Level::Level(int width, int height, int document_doors): width(width), height(height), document_doors(document_doors)
 {
 }
 
@@ -14,11 +55,22 @@ Level::~Level()
 		delete lo;
 }
 
+int Level::size()
+{
+	return this->width * this->height;
+}
+
 void Level::add_elevator(Elevator* e)
 {
 	for (int i = e->min_floor; i <= e->max_floor; i++)
-		this->elevator_occupied.insert(std::pair<int, int>(e->x, i));
+		this->occupied.insert(std::pair<int, int>(e->x, i));
 	this->objects.push_back(e);
+}
+
+void Level::add_xy(SingleFloorLevelObject * sflo)
+{
+	this->occupied.insert(std::make_pair(sflo->x, sflo->y));
+	this->objects.push_back(sflo);
 }
 
 void Level::process(float delta)
@@ -37,6 +89,6 @@ void Level::build()
 {
 	if (this->built)
 		throw std::exception("Level has already been built");
-	// TODO: Generate missing map objects
-	this->built = true;
+	this->generate_missing();
+	//this->built = true;
 }
--- ElevatorAction/ElevatorAction/Level.hpp
+++ ElevatorAction/ElevatorAction/Level.hpp
@@ -3,20 +3,29 @@
 #include <set>
 #include <utility>
 #include "LevelObject.hpp"
+#include "SingleFloorLevelObject.hpp"
 #include "Elevator.hpp"
 class Level
 {
 	std::vector<LevelObject*> objects = std::vector<LevelObject*>();
-	std::set<std::pair<int, int>> elevator_occupied = std::set<std::pair<int, int>>();
+	std::set<std::pair<int, int>> occupied = std::set<std::pair<int, int>>();
 	bool built = false;
+
+	int remaining_free();
+	void generate_missing();
+	void generate_missing_doors();
+	void insert_document_doors();
 public:
 	int width;
 	int height;
+	int document_doors;
 
-	Level(int width, int height);
+	Level(int width, int height, int document_doors);
 	~Level();
 
+	int size();
 	void add_elevator(Elevator *e);
+	void add_xy(SingleFloorLevelObject *sflo);
 	void process(float delta);
 	void render(float delta);
 	void build();
