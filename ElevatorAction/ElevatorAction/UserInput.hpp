e9a22b2d9ef89ebefbd9a1739aa52352abb090af
Aleksandar Kondic <konda96@gmail.com>
2019-09-01 21:58:24 +0200

    Refactor UserInput

--- ElevatorAction/ElevatorAction/UserInput.hpp
+++ ElevatorAction/ElevatorAction/UserInput.hpp
@@ -2,11 +2,11 @@
 class UserInput
 {
 public:
-	static const int UP = 0;
-	static const int DOWN = 1;
-	static const int LEFT = 2;
-	static const int RIGHT = 3;
-	static const int USE = 4;
+	bool up = false;
+	bool down = false;
+	bool left = false;
+	bool right = false;
+	bool use = false;
 	UserInput();
 	~UserInput();
 };
