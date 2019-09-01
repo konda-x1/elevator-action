09664a27737face3b2dd8b47b7ccab907f1d7afa
Aleksandar Kondic <konda96@gmail.com>
2019-09-01 19:32:26 +0200

    Add player input command indicators and their basic handling

--- ElevatorAction/ElevatorAction/Player.cpp
+++ ElevatorAction/ElevatorAction/Player.cpp
@@ -23,9 +23,20 @@ void Player::check_usable()
 		throw std::exception("Level is null");
 }
 
+void Player::reset_inputs()
+{
+	this->input_up = false;
+	this->input_down = false;
+	this->input_left = false;
+	this->input_right = false;
+	this->input_fire = false;
+}
+
 void Player::process(float delta)
 {
 	this->check_usable();
+
+	this->reset_inputs();
 }
 
 void Player::render(float delta)
--- ElevatorAction/ElevatorAction/Player.hpp
+++ ElevatorAction/ElevatorAction/Player.hpp
@@ -11,12 +11,18 @@ public:
 	float width = 0.2f, height = 0.5f;
 	Level *level = nullptr;
 	PlayerHitbox *hitbox = nullptr;
+	bool input_up = false;
+	bool input_down = false;
+	bool input_left = false;
+	bool input_right = false;
+	bool input_fire = false;
 
 	Player();
 	Player(Level *level, float fx, float fy);
 	~Player();
 
 	void check_usable() override;
+	void reset_inputs();
 	void process(float delta);
 	void render(float delta);
 };
